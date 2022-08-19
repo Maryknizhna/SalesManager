54a4c292e3bde963eb765c7236bd8893ee81e856
Maria Schevtsova <hatulmadan5002@gmail.com>
2022-08-19 13:43:49 +0300

    добавлены ридми и меин

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Main {
+public class Main  {
+    public static void main(String[] args) {
+        SalesManager salesManager = new SalesManager(new int[]{3,4,5});
+        System.out.println("Наибольшее кол-во: " + salesManager.max());
+    }
 }
+
+
