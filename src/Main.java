54b79b5617824c9843c91821422b390d35a83a03
Maria Schevtsova <hatulmadan5002@gmail.com>
2022-08-24 12:37:23 +0300

    long

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main  {
     public static void main(String[] args) {
-        SalesManager salesManager = new SalesManager(new int[]{3,4,5});
+        SalesManager salesManager = new SalesManager(new long[]{3,4,5});
         System.out.println("Наибольшее кол-во: " + salesManager.max());
     }
 }
