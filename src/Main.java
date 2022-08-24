ba4494e43bbcef436939944d05f85f351f5084e9
Maria Schevtsova <hatulmadan5002@gmail.com>
2022-08-24 13:20:58 +0300

    medium

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main  {
     public static void main(String[] args) {
-        SalesManager salesManager = new SalesManager(new int[]{3,4,5});
+        SalesManager salesManager = new SalesManager(new int[]{3,4,5,6,7,8});
+        int medium = (4 + 5 + 6 + 7)/4;
         System.out.println("Наибольшее кол-во: " + salesManager.max());
+        System.out.println("Среднее кол-во продаж: " + medium);
     }
 }
 
